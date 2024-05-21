0be0dc0997e7ce37a8ebcb4d3a5f87b30e258226
vicvergara <vvergara@ahorra.io>
2024-05-21 01:30:09 -0600

    peticion get

--- src/composables/services.js
+++ src/composables/services.js
@@ -2,18 +2,19 @@ import axios from 'axios'
 
 
 
-export function servicios(table){
+export function servicios(){
     const baseId = "appl7VZtgQ5DKAdVZ"
     const baseURL = "https://api.airtable.com/v0/"
+    const token = 'pathpGJJfDkmLCGgm.0760accdd0d574675d351d343cc90cfedc326a3d9776059611cc52561bf641e8'
 
-    const get = async () => {
+    const get = async (table) => {
         const response = await axios({
             method:'get',
             headers: {
                 'Content-Type': 'application/json',
                 Authorization: 'Bearer ' + token
             },
-            url: `${baseURL}/${baseId}/${table}`
+            url: `${baseURL}${baseId}/${table}`
         }).then(res => res)
         return response.data        
     }
@@ -28,4 +29,6 @@ export function servicios(table){
         }).then(res => res)
         return response.data        
     }
+
+    return {get,post}
 }
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,5 +1,17 @@
 <template>
     <main>
-        <div>dashboard</div>
+        <div>
+            <button @click="getDatos()">Datos</button>
+        </div>
     </main>
 </template>
+
+<script setup>
+import {servicios} from "@/composables/services.js";
+const {get,post} = servicios()
+
+function getDatos() {
+    get('tblKUeL6mByctJU0L')
+}
+
+</script>
